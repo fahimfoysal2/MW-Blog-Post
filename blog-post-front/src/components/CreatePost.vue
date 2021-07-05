30f4fedb91672fa917ce09a2b3fe186206361af1
Fahim Foysal <ffoysal6@gmail.com>
2021-07-05 14:43:54 +0600

    Component for Creating a new post

---
+++ blog-post-front/src/components/CreatePost.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="create-post">
+        <form>
+            <div>
+                <textarea></textarea>
+            </div>
+            <button>Create Post</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CreatePost"
+}
+</script>
+
+<style scoped>
+
+</style>
