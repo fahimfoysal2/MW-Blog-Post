2e97bc234a5d6693fdbdf9ae9756b342000424f8
Fahim Foysal <ffoysal6@gmail.com>
2021-07-05 18:11:29 +0600

    Login/ Registration Page Created

--- blog-post-front/src/router/index.js
+++ blog-post-front/src/router/index.js
@@ -2,6 +2,7 @@ import Vue       from 'vue'
 import VueRouter from 'vue-router'
 import Home      from '../views/Home.vue'
 import Post      from "@/views/Post";
+import Login     from "@/views/Login";
 
 Vue.use(VueRouter)
 
@@ -25,6 +26,11 @@ const routes = [
         path     : '/post/:id',
         name     : 'Post',
         component: Post
+    },
+    {
+        path: "/login",
+        name: 'Login',
+        component: Login,
     }
 ]
 
---
+++ blog-post-front/src/views/Login.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="login">
+
+        <h3>Login Page</h3>
+        <form v-on:submit.prevent>
+            <input v-model.trim="email" type="email" name="email" id="email" placeholder="Email"> <br> <br>
+            <input v-model="password" type="password" name="password" id="pass" placeholder="Password"> <br> <br>
+
+            <div v-if="newUser">
+                <button @click="authenticate">Register</button>
+                or, <a href="#" @click="newUser = false">Login</a>
+            </div>
+
+            <div v-else>
+                <button @click="authenticate">Login</button>
+                or, <a href="#" @click="newUser = true">Create Account</a>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Login",
+    data() {
+        return {
+            newUser : false,
+            email   : '',
+            password: '',
+        }
+    },
+    methods: {
+        authenticate() {
+            console.log('Authentication Process');
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
