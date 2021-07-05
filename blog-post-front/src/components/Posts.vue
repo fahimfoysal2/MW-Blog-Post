d9a7e9686442018cac0d961d3676c59e635e4a7f
Fahim Foysal <ffoysal6@gmail.com>
2021-07-05 14:42:42 +0600

    'Posts' Component to show posts list

---
+++ blog-post-front/src/components/Posts.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="posts">
+        <div v-for="post in posts" class="post">
+            <h1>{{ post.title }}</h1>
+            <small>
+                <a href="#">
+                    {{ post.author.name }}
+                </a>
+            </small>
+            <div class="post-text">
+                <p>
+                    {{ post.content }}
+                    <span class="read-more">
+                    <a href="#">Read More</a>
+                </span>
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'PostList',
+    props: {
+        posts: Array
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.posts {
+    text-align: unset;
+}
+
+.post {
+    border-bottom: 1px solid #2c3e50;
+    padding: 0 15px;
+}
+
+.post:last-child {
+    border: none;
+}
+
+.post-text {
+    text-align: left;
+}
+
+h1, h3 {
+    margin: 40px 0 0;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42b983;
+}
+</style>
