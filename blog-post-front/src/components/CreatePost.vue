7fb675a0b54f9e45747c35cea163dfb8fba6004e
Fahim Foysal <ffoysal6@gmail.com>
2021-07-06 19:21:01 +0600

    Create New Post

--- blog-post-front/src/components/CreatePost.vue
+++ blog-post-front/src/components/CreatePost.vue
@@ -1,17 +1,42 @@
 <template>
     <div class="create-post">
-        <form>
+        <form v-on:submit.prevent>
+            <input type="text" placeholder="title" v-model="newPost.title">
             <div>
-                <textarea></textarea>
+                <textarea v-model="newPost.details"></textarea>
             </div>
-            <button>Create Post</button>
+            <button @click="createNewPost">Create Post</button>
         </form>
     </div>
 </template>
 
 <script>
 export default {
-    name: "CreatePost"
+    name: "CreatePost",
+    data() {
+        return {
+            newPost: {
+                title  : '',
+                details: '',
+            }
+        }
+    },
+    methods: {
+        createNewPost() {
+            axios.post('http://127.0.0.1:8000/api/posts',
+                this.newPost,
+                {
+                    headers: {'Authorization': 'Bearer ' + this.$store.state.token}
+                })
+                .then(response => {
+                    console.log("Post Created");
+                    console.log(response.data);
+                })
+                .catch(reason => {
+                    console.log(reason.data);
+                });
+        }
+    }
 }
 </script>
 
